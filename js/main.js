de66b51d8cbf71a4752a3ef454f8a657a6125d42
Lee <liam.eakins@outlook.com>
2018-12-26 03:59:41 -0600

    Complete project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 var $ball = $('.ball');
 
+$('#btn-up').on('click', function() {
+    var newDown = $ball.offset().top - 10;
+    $ball.css('top', newDown);
+});
+
+
+
+
 $('#btn-right').on('click', function() {
     var newLeft = $ball.offset().left + 10;
     $ball.css('left', newLeft);
+});
+
+
+$('#btn-down').on('click', function() {
+    var newTop = $ball.offset().top + 10;
+    $ball.css('top', newTop);
+});
+
+
+
+$('#btn-left').on('click', function() {
+    var newRight = $ball.offset().left - 10;
+    $ball.css('left', newRight);
 });
